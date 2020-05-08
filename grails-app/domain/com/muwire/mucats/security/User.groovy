a1c198a191bb64ca98273e731c5abc2fc4473f9c
Zlatin Balevsky <zlatinb@gmail.com>
2020-05-08 15:44:00 +0100

    get rid of the password field

--- grails-app/domain/com/muwire/mucats/security/User.groovy
+++ grails-app/domain/com/muwire/mucats/security/User.groovy
@@ -12,22 +12,14 @@ class User implements Serializable {
     private static final long serialVersionUID = 1
 
     String username
-    String password
     boolean enabled = true
-    boolean accountExpired
     boolean accountLocked
-    boolean passwordExpired
 
     Set<Role> getAuthorities() {
         (UserRole.findAllByUser(this) as List<UserRole>)*.role as Set<Role>
     }
 
     static constraints = {
-        password nullable: false, blank: false, password: true
         username nullable: false, blank: false, unique: true
-    }
-
-    static mapping = {
-	    password column: '`password`'
     }
 }
